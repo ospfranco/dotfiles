bad96d490c3cbff0c5febc12b39270873ad32777
Oscar Franco <ospfranco@gmail.com>
2025-10-22 14:01:12 +0200

    Configs

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,2 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.keymap.set("n", "<D-s>", "<cmd>w<cr>", { desc = "Save file" })
+vim.keymap.set("i", "<D-s>", "<esc><cmd>w<cr>a", { desc = "Save file" })
