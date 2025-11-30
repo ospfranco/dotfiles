f933daa6a7a74f411f96e4e825b192774bdb82b2
Oscar Franco <ospfranco@gmail.com>
2025-11-30 15:35:19 +0100

    update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<D-s>", "<cmd>w<cr>", { desc = "Save file" })
 vim.keymap.set("i", "<D-s>", "<esc><cmd>w<cr>a", { desc = "Save file" })
+vim.keymap.set("n", "<D-w>", "<cmd>bd!<cr>", { desc = "Close Buffer" })
+
